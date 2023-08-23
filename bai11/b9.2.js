f7929fda541c84a2ea4c935c81169b84186fa69c
unknown <tgdd.hieu2005@gmail.com>
2023-08-23 22:18:49 +0700

    bai11

---
+++ bai11/b9.2.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+    for (let a = 10; a >= i; a--) {
+        document.write("*");
+        document.write("&nbsp&nbsp&nbsp");
+    }
+    document.write("<br>");
+} 
---
+++ bai11/b9.js
@@ -0,0 +1,11 @@
+
+for (let i = 1; i <= 10; i++) {
+    for (let a = 1; a <= i; a++) {
+        document.write("*");
+        document.write("&nbsp&nbsp&nbsp&nbsp");
+    }
+    document.write("<br>");
+}
+ 
+
+
